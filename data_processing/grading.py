035d25a14d91330e65f30145c34f71db0fbe65b6
Szymon Skarzyński <rosomak909@gmail.com>
2021-12-04 07:07:09 +0100

    added grading of whole project

--- data_processing/grading.py
+++ data_processing/grading.py
@@ -6,9 +6,21 @@ WEIGHT_VERSION_COUNT = 0.1
 WEIGHTS_SUM = WEIGHT_LAST_UPDATE + WEIGHT_VERSION_COUNT
 # WEIGHT_LAST_FREQUENCY = 0.1
 
+
+def grade_project(project_dependencies) -> float:
+    grade = sum(
+        [
+            grade_dependency(dependency)["overall_grade"] for dependency
+            in project_dependencies.dependencies
+        ]
+    )
+
+    return grade
+
+
 def grade_dependency(dependency) -> float:
     last_update_grade = grade_last_update(dependency)
-    version_count_grade = grade_version_count(dependency) 
+    version_count_grade = grade_version_count(dependency)
     overall_grade = sum([
         last_update_grade * WEIGHT_LAST_UPDATE,
         version_count_grade * WEIGHT_VERSION_COUNT
@@ -20,11 +32,12 @@ def grade_dependency(dependency) -> float:
         "version_count_grade": version_count_grade
     }
 
+
 def grade_last_update(dependency) -> float:
     timestamp = dependency.timestamp
     seconds_from_timestamp = time.time() - timestamp
     days_from_timestamp = (
-        seconds_from_timestamp / 60 / 60 / 24 
+        seconds_from_timestamp / 60 / 60 / 24
     )
 
     max_days = 30
@@ -39,11 +52,11 @@ def grade_last_update(dependency) -> float:
 
 def grade_version_count(dependency) -> float:
     version_count = dependency.version_count
-    
+
     first_version_timestamp = None
     seconds_from_first = time.time() - first_version_timestamp
     days_from_first = (
-        seconds_from_first / 60 / 60 / 24 
+        seconds_from_first / 60 / 60 / 24
     )
 
     mean_period = days_from_first / version_count
@@ -59,11 +72,11 @@ def grade_version_count(dependency) -> float:
 
 # def grade_update_frequency(dependency) -> float:
 #     version_count = dependency.version_count
-    
+
 #     first_version_timestamp = None
 #     seconds_from_first = time.time() - first_version_timestamp
 #     days_from_first = (
-#         seconds_from_first / 60 / 60 / 24 
+#         seconds_from_first / 60 / 60 / 24
 #     )
 
 #     mean_period = days_from_first / version_count
@@ -76,5 +89,3 @@ def grade_version_count(dependency) -> float:
 #         grade = 1 - (mean_period) / max_period
 
 #     return grade
-
-
