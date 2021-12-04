f676896d55e974bc0af230b76551978a77f42463
Szymon Skarzyński <rosomak909@gmail.com>
2021-12-04 07:05:05 +0100

    beautifying ask_api_package_details function

--- data_processing/api_management.py
+++ data_processing/api_management.py
@@ -4,9 +4,10 @@ This module is responsible for api connection
 
 import requests
 
+
 def ask_api_package_details(group_id: str, artifact_id: str):
     api_request = f"https://search.maven.org/solrsearch/select?q=g:%22{group_id}%22%20AND%20a:%22{artifact_id}%22&rows=20&wt=json"
-    
+
     response = requests.get(api_request)  # ask for request
-    
+
     return response.json()
